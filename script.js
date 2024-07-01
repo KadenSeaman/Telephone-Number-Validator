cac8f1bb7318f8dbc7596cf34dcd2075ed8660d1
KadenSeaman <kadencs24@outlook.com>
2024-07-01 17:36:56 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+//Variables
+const checkButton = document.getElementById("check-btn");
+const clearButton = document.getElementById("clear-btn");
+const userInput = document.getElementById("user-input");
+const results = document.getElementById("results-div")
+
+//Methods & Functions
+const phoneNumberIsValid = () => {
+    const pattern = /^(1?\s?([0-9]{3}|\([0-9]{3}\))\s?-?\s?[0-9]{3}\s?-?[0-9]{4})$/
+    return pattern.test(userInput.value);
+}
+
+
+//Inits & EventListeners
+clearButton.addEventListener("click",(e) => {
+    userInput.value="";
+    e.preventDefault();
+
+
+})
+
+checkButton.addEventListener("click", (e) =>{
+    e.preventDefault();
+
+    if (userInput.value === ""){
+        alert("Please provide a phone number")
+    }
+
+    if(phoneNumberIsValid()) {
+        console.log("yes");
+    }
+    else{
+        console.log("no");
+    }
+})
